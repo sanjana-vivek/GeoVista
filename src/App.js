8cf2ccd9b8807981b6f135822018b3c451d3389c
Sanjana Vivek <sanjana@vivekv.info>
2024-10-05 18:44:58 +0530

    Update to App file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GeoVista</h1>
+      <h3>Your personalised water pollution detector</h3>
+      <p>
+        Anywhere, everywhere! Let's uplift our country's might rivers! Our
+        water polution index and warnings are facilitated by spectral analysis
+        of Landsat Reflectance data
+      </p>
     </div>
   );
 }
